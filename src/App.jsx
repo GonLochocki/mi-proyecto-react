ab9059b8925c88aa93c763ff1ff9eef29297457e
Gonzalo Lochocki <gonlochocki@gmail.com>
2023-06-16 13:59:27 -0300

    modulos de css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
+import styles from "./App.module.css";
+
 const App = () => {
-  let nombre = "Liam";
-
-  const saludar = () => {
-    console.log("Hola", nombre);
-  };
-
   return (
     <div>
-      <h1>Hola {nombre}</h1>
-      <button onClick={saludar}>Saludo</button>
+      <h1 className={styles.strong}>Este es el App</h1>;
+      <img
+        src="https://w7.pngwing.com/pngs/804/706/png-transparent-footwear-shoe-clothing-valco-plast-sneakers-others-miscellaneous-logo-vector-icons.png"
+        alt=""
+      />
     </div>
   );
 };
